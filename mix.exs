763b8724f01c2cdfb5a9dcd3d58390ba12f7563a
Robin Hilliard <robin@rocketboots.com>
2020-12-26 17:41:02 +1100

    mix fixtures alias add compiled fixtures to project

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Safeish.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -24,5 +25,25 @@ defmodule Safeish.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+  
+  defp aliases, do: [fixtures: &make_fixtures/1]
+  
+  defp make_fixtures(_) do
+    source_path =  "#{File.cwd!()}/test/fixtures.ex"
+    build_path = "#{File.cwd!()}/test/fixtures_build"
+    
+    Mix.Shell.IO.info("Compiling modules in #{source_path} and saving to #{build_path}")
+    
+    File.rm_rf(build_path)
+    File.mkdir(build_path)
+    
+    for {module, bytecode} <- Code.compile_file(source_path) do
+      Mix.Shell.IO.info("...#{build_path}/#{Atom.to_string(module)}.beam")
+      {:ok, file} = File.open("#{build_path}/#{Atom.to_string(module)}.beam", [:write])
+      IO.binwrite(file, bytecode)
+      File.close(file)
+    end
+    
   end
 end
---
+++ test/fixtures.ex
@@ -0,0 +1,44 @@
+# run `mix fixtures` to clean and re-compile
+
+defmodule ModuleCallsEnumMap do
+  def f() do
+    Enum.map([1, 2, 3], fn x -> x + 1 end)
+  end
+end
+
+defmodule ModuleCallsFileRead do
+  def f() do
+    File.read("nofile.txt")
+  end
+end
+
+
+defmodule ModuleSpawnsProcess do
+  def f() do
+    spawn(ModuleSpawnsProcess, fn x -> x end, [])
+  end
+end
+
+
+defmodule ModuleReceivesMessage do
+  def f() do
+    receive do
+      _ ->
+        true
+    end
+  end
+end
+
+
+defmodule ModuleSendsMessage do
+  def f() do
+    send self(), :msg
+  end
+end
+
+
+defmodule ModuleCallsToAtom do
+  def f() do
+    String.to_atom("not_allowed")
+  end
+end
