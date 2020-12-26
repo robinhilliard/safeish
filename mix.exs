a926c7a3748b7c67e50cd24e59b0adf60b4d0106
Robin Hilliard <robin@rocketboots.com>
2020-12-26 18:45:08 +1100

    add ex_doc

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Safeish.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
   
   defp description() do
